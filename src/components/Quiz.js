37cb931ec34ed6720b9105d2de18d1ac2483f556
Karl George <karlgdev@gmail.com>
2022-04-20 11:55:01 +0200

    Contacting api

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,21 @@
+import { useState, useEffect } from 'react';
+
 const Quiz = () => {
+  const [quizContent, setQuizContent] = useState([]);
+
+  useEffect(() => {
+    async function getQuizContent() {
+      const res = await fetch(
+        'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple'
+      );
+      const data = await res.json();
+      setQuizContent(data.results);
+    }
+
+    getQuizContent();
+  }, []);
+
+  console.log(quizContent);
   return <div>Quiz</div>;
 };
 
