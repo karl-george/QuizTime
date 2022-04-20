f5141a5740391717faa2c92e0e962da51e2d13d5
Karl George <karlgdev@gmail.com>
2022-04-20 13:27:57 +0200

    Styling quiz sheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Splash from './components/Splash';
 import Quiz from './components/Quiz';
 import './App.css';
 
 function App() {
   const [splash, setSplash] = useState(true);
+  const [quizContent, setQuizContent] = useState([]);
+
+  useEffect(() => {
+    async function getQuizContent() {
+      const res = await fetch(
+        'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple'
+      );
+      const data = await res.json();
+      setQuizContent(data.results);
+    }
+
+    getQuizContent();
+  }, []);
 
   const removeSplash = () => {
     setSplash(false);
@@ -12,7 +25,11 @@ function App() {
 
   return (
     <div className='App'>
-      {splash ? <Splash handleClick={removeSplash} /> : <Quiz />}
+      {splash ? (
+        <Splash handleClick={removeSplash} />
+      ) : (
+        <Quiz quizContent={quizContent} />
+      )}
     </div>
   );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,22 +1,34 @@
 import { useState, useEffect } from 'react';
+import '../styles/Quiz.css';
 
-const Quiz = () => {
-  const [quizContent, setQuizContent] = useState([]);
-
-  useEffect(() => {
-    async function getQuizContent() {
-      const res = await fetch(
-        'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple'
-      );
-      const data = await res.json();
-      setQuizContent(data.results);
-    }
-
-    getQuizContent();
-  }, []);
-
+const Quiz = ({ quizContent }) => {
   console.log(quizContent);
-  return <div>Quiz</div>;
+
+  return (
+    <div>
+      <h2 className='question'>{quizContent[0].question}</h2>
+      <ul className='answers'>
+        <li>
+          <input type='radio' name='answer' id='answer1' />
+          <label htmlFor='answer1'>{quizContent[0].correct_answer}</label>
+        </li>
+        <li>
+          <input type='radio' name='answer' id='answer2' />
+          <label htmlFor='answer2'>{quizContent[0].incorrect_answers[0]}</label>
+        </li>
+        <li>
+          <input type='radio' name='answer' id='answer3' />
+          <label htmlFor='answer3'>{quizContent[0].incorrect_answers[1]}</label>
+        </li>
+        <li>
+          <input type='radio' name='answer' id='answer4' />
+          <label htmlFor='answer4'>{quizContent[0].incorrect_answers[2]}</label>
+        </li>
+      </ul>
+
+      <button className='btn btn-filled'>Check Answers</button>
+    </div>
+  );
 };
 
 export default Quiz;
