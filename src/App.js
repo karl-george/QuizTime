48361773359d3d96e443067f888f57498a56c94a
Karl George <karlgdev@gmail.com>
2022-04-22 11:40:49 +0200

    Working on tallying answers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import { useState, useEffect } from 'react';
 import Splash from './components/Splash';
-import Quiz from './components/Quiz';
+import Question from './components/Question';
 import './App.css';
 
 function App() {
   const [splash, setSplash] = useState(true);
   const [quizContent, setQuizContent] = useState([]);
+  const [tallyPoints, settallyPoints] = useState({
+    correctAnswers: 0,
+    submitted: false,
+  });
+  const [userAnswers, setUserAnswers] = useState({
+    0: '',
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+  });
+
+  async function getQuizContent() {
+    const res = await fetch(
+      'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple'
+    );
+    const data = await res.json();
+    setQuizContent(data.results);
+  }
 
   useEffect(() => {
-    async function getQuizContent() {
-      const res = await fetch(
-        'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple'
-      );
-      const data = await res.json();
-      setQuizContent(data.results);
-    }
-
     getQuizContent();
   }, []);
 
@@ -23,12 +34,81 @@ function App() {
     setSplash(false);
   };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserAnswers((prevAnswers) => {
+      return {
+        ...prevAnswers,
+        [name]: value,
+      };
+    });
+  };
+
+  const quizElement = quizContent.map((question_answer, index) => {
+    return (
+      <Question
+        data={question_answer}
+        key={index}
+        index={index}
+        handleChange={handleChange}
+      />
+    );
+  });
+
+  const checkAnswers = () => {
+    if (!tallyPoints.submitted) {
+      for (let i = 0; i < quizContent.length; i++) {
+        if (userAnswers[i] === quizContent[i].correct_answer) {
+          settallyPoints((prev) => {
+            return {
+              ...prev,
+              correctAnswers: prev.correctAnswers + 1,
+            };
+          });
+        }
+      }
+
+      settallyPoints((prev) => {
+        return {
+          ...prev,
+          submitted: true,
+        };
+      });
+    } else {
+      getQuizContent();
+      setUserAnswers({
+        0: '',
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+      });
+      settallyPoints(() => {
+        return {
+          correctAnswers: 0,
+          submitted: false,
+        };
+      });
+    }
+  };
+
   return (
     <div className='App'>
       {splash ? (
         <Splash handleClick={removeSplash} />
       ) : (
-        <Quiz quizContent={quizContent} />
+        <div>
+          {quizElement}
+          {tallyPoints.submitted ? (
+            <p>{`You scored ${tallyPoints.correctAnswers}/5 correct answers`}</p>
+          ) : (
+            ''
+          )}
+          <button className='btn btn-filled' onClick={checkAnswers}>
+            {tallyPoints.submitted ? `Play Again` : `Check Answers`}
+          </button>
+        </div>
       )}
     </div>
   );
